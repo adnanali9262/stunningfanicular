c17742f08f7d5e0cd53ef97516c346987fd65181
adnanali9262 <adnan.rafiq173@GMAIL.COM>
2025-09-18 15:22:12 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = "energy-calc-v2";
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('app-cache-v2').then((cache) => {
-      return cache.addAll(['./', './index.html']);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
